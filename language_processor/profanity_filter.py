1f5496c0bc663c3c8f35a234c744af6a62138247
Joshua-K1 <joshuack1996@gmail.com>
2024-04-21 22:44:50 +0100

    Asserting value is string before checking for profanity

--- language_processor/profanity_filter.py
+++ language_processor/profanity_filter.py
@@ -4,3 +4,8 @@ def load_censor():
     print("Loading censor words..")
     return profanity.load_censor_words()
 
+def check_profanity(text):
+    print("Check profanity..")
+    
+    # returns True if profanity is found
+    return profanity.contains_profanity(text)
--- lex.py
+++ lex.py
@@ -1,9 +1,9 @@
 from data_factory import factory
 from language_processor import tokeniser as lp
 from language_processor import profanity_filter
+from better_profanity import profanity
 
 def main():
-    pf = profanity_filter.load_censor()
 
     print("Main...")
 
@@ -13,9 +13,29 @@ def main():
     # extract keys and values from json data
     extracted_values = factory.load_keys(data)
 
+    # create dataframe
     df = factory.create_dataframe(extracted_values)
-    print(df)
 
+    keys_with_profanity = check_profanity(extracted_values)
+
+    if keys_with_profanity is not None:
+        for key in keys_with_profanity:
+            print(key)
+    else:
+        print("No keys with profanity found...")
+
+
+# check keys for profanity 
+def check_profanity(input):
+    print("Check for profanity...")
+    keys_with_profanity = []
+    for value in input:
+        if isinstance(value['Value'], str):
+            if profanity.contains_profanity(value['Value']):
+                print(f"{value['Key']} contains profanity")
+
+    
+    return keys_with_profanity
 
 
 if __name__ == "__main__":
